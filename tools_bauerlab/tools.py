efe674915128232f176bf83936f1e5bfab5f1705
tristan <tristan.stoeber@posteo.net>
2021-02-03 10:47:32 +0100

    asdf

--- tools_bauerlab/tools.py
+++ tools_bauerlab/tools.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pathlib import Path
 
 def generate_default_re():
     
@@ -29,9 +30,13 @@ def generate_fileinfo_table(path, re=None):
     Generate file info table by recursively scanning for all files in path
     that match regular expression
     """
-    
+
     if not re:
         re = generate_default_re()
+
+    ls_fname = [str(i.absolute()) for i in Path(path).rglob('*')]
+    df = pd.DataFrame({'fname': ls_fname})
+        
     df_extract = df['fname'].str.extract(re, expand=True)
 
     df_extract = df_extract.rename(
@@ -45,6 +50,7 @@ def generate_fileinfo_table(path, re=None):
     df = df.join(df_extract)
 
     df = df.dropna()
-
+    df = df.reset_index(drop=True)
+    
     return df
         
